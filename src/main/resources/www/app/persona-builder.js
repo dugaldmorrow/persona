4befcdfa817218691b7d817b5538295246f0eeb6
Dugald Morrow <dugaldmorrow@MacBook-Pro.fritz.box>
2015-04-19 20:51:29 +1000

    Initial spiking.

--- src/main/resources/www/app/persona-builder.js
+++ src/main/resources/www/app/persona-builder.js
@@ -3,6 +3,8 @@ define(function (svg) {
 	var $ = require('jquery');
 	var width = 400;
 	var height = 400;
+//	var width = 16;
+//	var height = 16;
 	var centreX = width / 2;
 	var centreY = height / 2;
 	var faceWidth = width;
@@ -10,6 +12,9 @@ define(function (svg) {
 	var eyeOffsetX = centreX / 3;
 	var leftEyeCentreX = centreX - eyeOffsetX;
 	var rightEyeCentreX = centreX + eyeOffsetX;
+	var cheekOffsetX = centreX / 2;
+	var leftCheekCentreX = centreX - cheekOffsetX;
+	var rightCheekCentreX = centreX + cheekOffsetX;
 	
 	var Emotion = function(active) {
 		this.active = active;
@@ -23,6 +28,7 @@ define(function (svg) {
 	function Eye(active, eyeCentreX) {
 		Emotion.call(this, active);
 		this.eyeCentreX = eyeCentreX;
+		this.eyeCentreY = 9 * centreY / 10;
 		this.wink = false;
 	};
 	Eye.prototype = Object.create(Emotion.prototype);
@@ -34,21 +40,58 @@ define(function (svg) {
 		var eyeRadius = width / 5;
     	if (this.wink) {
         	var eyeHeightY = eyeRadius / 2;
-        	persona.ellipse(eyeRadius, eyeHeightY).fill('#ffffff').stroke('#000000').move(this.eyeCentreX - eyeRadius / 2, centreY - eyeHeightY / 2);
+        	persona.ellipse(eyeRadius, eyeHeightY).fill('#ffffff').stroke('#000000').move(this.eyeCentreX - eyeRadius / 2, this.eyeCentreY - eyeHeightY / 2);
         } else {
-        	persona.circle(eyeRadius).fill('#ffffff').stroke('#000000').move(this.eyeCentreX - eyeRadius / 2, centreY - eyeRadius / 2);
+        	persona.circle(eyeRadius).fill('#ffffff').stroke('#000000').move(this.eyeCentreX - eyeRadius / 2, this.eyeCentreY - eyeRadius / 2);
+        }
+	};
+	
+	// Cheek...
+	function Cheek(active, cheekCentreX) {
+		Emotion.call(this, active);
+		this.EMBARRASSED_COLOR = '#de929d';
+		this.cheekCentreX = cheekCentreX;
+		this.cheekCentreY = 25 * centreY / 20;
+		this.blushed = false;
+	};
+	Cheek.prototype = Object.create(Emotion.prototype);
+	Cheek.prototype.constructor = Cheek;
+	Cheek.prototype.reset = function() {
+		this.blushed = false;
+	};
+	Cheek.prototype.blush = function() {
+		this.blushed = true;
+	};
+	Cheek.prototype.render = function(persona) {
+		var cheekWidth = width / 10;
+		var cheekHeight = width / 8;
+    	if (this.blushed) {
+        	persona.ellipse(cheekWidth, cheekHeight).fill(this.EMBARRASSED_COLOR).stroke(this.EMBARRASSED_COLOR).move(this.cheekCentreX - cheekWidth / 2, this.cheekCentreY - cheekHeight / 2);
+        } else {
         }
 	};
 	
 	// Mouth...
 	function Mouth(active) {
 		Emotion.call(this, active);
-		this.smile = false;
+		this.STRAIGHT_STYLE = 'straight';
+		this.FROWN_STYLE = 'frown';
+		this.SMILE_STYLE = 'smile';
+		this.CONFUSED_STYLE = 'confused';
 	};
 	Mouth.prototype = Object.create(Emotion.prototype);
 	Mouth.prototype.constructor = Mouth;
-	Mouth.prototype.setSmile = function(smile) {
-		this.smile = smile;
+	Mouth.prototype.straight = function() {
+		this.style = this.STRAIGHT_STYLE;
+	};
+	Mouth.prototype.frown = function() {
+		this.style = this.FROWN_STYLE;
+	};
+	Mouth.prototype.smile = function() {
+		this.style = this.SMILE_STYLE;
+	};
+	Mouth.prototype.confused = function() {
+		this.style = this.CONFUSED_STYLE;
 	};
 	Mouth.prototype.render = function(persona) {
 		var mouthWidth = width / 2;
@@ -56,11 +99,30 @@ define(function (svg) {
 		var mouthCentreY = faceHeight * 3 / 4;
 		var mouthLeftX = centreX - mouthWidth / 2;
 		var mouthRightX = mouthLeftX + mouthWidth;
-		if (this.smile) {
+		if (this.style == this.FROWN_STYLE) {
+			var mouthBottomY = mouthCentreY - faceHeight / 10;
+			var path = persona.path(
+					'M ' + mouthLeftX + ' ' + mouthCentreY +
+					' C ' + mouthLeftX + ' ' + mouthCentreY + ' ' + centreX + ' ' + mouthBottomY + ' ' + mouthRightX + ' ' + mouthCentreY);
+			path.fill('none').stroke({
+				width: 1
+			});
+		} else if (this.style == this.SMILE_STYLE) {
 			var mouthBottomY = mouthCentreY + faceHeight / 10;
 			var path = persona.path(
 					'M ' + mouthLeftX + ' ' + mouthCentreY +
 					' C ' + mouthLeftX + ' ' + mouthCentreY + ' ' + centreX + ' ' + mouthBottomY + ' ' + mouthRightX + ' ' + mouthCentreY);
+			path.fill('none').stroke({
+				width: 1
+			});
+		} else if (this.style == this.CONFUSED_STYLE) {
+			var mouthTopY = mouthCentreY - faceHeight / 10;
+			var mouthBottomY = mouthCentreY + faceHeight / 10;
+			var quarterWidth = mouthWidth / 4;
+			var path = persona.path(
+					'M ' + mouthLeftX + ' ' + mouthCentreY +
+					' C ' + mouthLeftX + ' ' + mouthCentreY + ' ' + (centreX - quarterWidth) + ' ' + mouthTopY + ' ' + centreX + ' ' + mouthCentreY +
+					' C ' + centreX + ' ' + mouthCentreY + ' ' + (centreX + quarterWidth) + ' ' + mouthBottomY + ' ' + mouthRightX + ' ' + mouthCentreY);
 			path.fill('none').stroke({
 				width: 1
 			});
@@ -93,6 +155,8 @@ define(function (svg) {
 			mouth: new Mouth(true),
 			leftEye: new Eye(true, leftEyeCentreX),
 			rightEye: new Eye(true, rightEyeCentreX),
+			leftCheek: new Cheek(true, leftCheekCentreX),
+			rightCheek: new Cheek(true, rightCheekCentreX),
 			boundingSize: width
 		},
 		
@@ -100,26 +164,41 @@ define(function (svg) {
 			//this.characteristics.mouth.setSmile(false);
         	this.characteristics.leftEye.setWink(false);
         	this.characteristics.rightEye.setWink(false);
+        	this.characteristics.leftCheek.reset();
+        	this.characteristics.rightCheek.reset();
         	return this;
 		},
-        
+		
+		// Actions...
         smile: function() {
-        	this.characteristics.mouth.setSmile(true);
+        	this.characteristics.mouth.smile();
         	return this;
         },
-        
         straightMouth: function() {
-        	this.characteristics.mouth.setSmile(false);
+        	this.characteristics.mouth.straight();
         	return this;
         },
-        
+        frown: function() {
+        	this.characteristics.mouth.frown();
+        	return this;
+        },
         winkLeft: function() {
         	this.characteristics.leftEye.setWink(true);
         	return this;
         },
-        
         winkRight: function() {
         	this.characteristics.rightEye.setWink(true);
+        	return this;
+        },
+        
+        // Emotions...
+        confused: function() {
+        	this.characteristics.mouth.confused();
+        	return this;
+        },
+        embarrassed: function() {
+        	this.characteristics.leftCheek.blush();
+        	this.characteristics.rightCheek.blush();
         	return this;
         },
         
@@ -133,8 +212,10 @@ define(function (svg) {
             var eyeCentreY = centreY - centreY / 6;
             var leftEyeCentreX = centreX - eyeOffsetX;
             var rightEyeCentreX = centreX + eyeOffsetX;
-            this.characteristics.rightEye.render(persona);
             this.characteristics.leftEye.render(persona);
+            this.characteristics.rightEye.render(persona);
+            this.characteristics.leftCheek.render(persona);
+            this.characteristics.rightCheek.render(persona);
             this.characteristics.mouth.render(persona);
         }
     };
